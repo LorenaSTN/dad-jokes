8cda11a9d58ef467819f9fb1347bd09c0729ea88
LorenaSTN <lorenstn@hotmail.com>
2024-11-29 18:17:33 +0100

    dark mode enabled

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@
   const paragraph = document.querySelector(".p-js");
   const button = document.querySelector(".button-js")
 
+// Api jokes
 
   const handleJoke = () =>{
   fetch("https://icanhazdadjoke.com/", {
@@ -14,16 +15,42 @@
     .then((response) => response.json())
     .then((data) => {
       if (paragraph) {
-        paragraph.innerHTML = data.joke;
+        paragraph.innerHTML = ` "${data.joke}"`;
       }
     })
     .catch((error) => console.error("Error:", error));
   }
 
-
     button?.addEventListener("click", handleJoke)
 
     handleJoke();
+
+// Dark mode
+
+let darkModeEnabled = false;
+
+const darkModeButton = document.querySelector(".darkMode-js");
+
+const handleDarkMode = () =>{
+  darkModeEnabled = !darkModeEnabled
+
+  if(darkModeEnabled){
+    enableDarkMode()
+  }else{
+    disableDarkMode()
+  }
+}
+
+darkModeButton?.addEventListener("click", handleDarkMode)
+
+const enableDarkMode = () => {
+  document.body.classList.add("dark-mode");
+}
+
+const disableDarkMode = () =>{
+  document.body.classList.remove("dark-mode");
+}
+
 })();
 
 
